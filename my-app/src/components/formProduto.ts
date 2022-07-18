82a63dbd381ca851e6b4530501bd5fd610a8dfed
Willian Costa <68620087+WillianCosta12@users.noreply.github.com>
2022-07-18 20:35:44 -0300

    primeiro commit

---
+++ my-app/src/components/formProduto.ts
@@ -0,0 +1,16 @@
+import React, { ReactNode, useState } from "react";
+import { isDOMComponent } from "react-dom/test-utils";
+import { useForm } from "react-hook-form";
+
+
+type Profile = {
+  titulo: string[];
+  conteudo: string[];
+};
+
+
+export function Form(){ 
+
+
+};
+export default Form;
---
+++ my-app/src/components/listProdutos.ts
@@ -0,0 +1,8 @@
+import React, { ReactNode, useState } from "react";
+import { isDOMComponent } from "react-dom/test-utils";
+
+export function List(){ 
+
+
+};
+export default List;
