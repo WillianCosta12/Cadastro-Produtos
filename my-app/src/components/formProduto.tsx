5cfcc7032f55330976369aeb491eb80a384d0f38
Willian Costa <68620087+WillianCosta12@users.noreply.github.com>
2022-07-20 10:12:30 -0300

    Validação e Cadastro

--- my-app/src/components/formProduto.tsx
+++ my-app/src/components/formProduto.tsx
@@ -24,8 +24,10 @@ export function Form(){
     "Bebidas",
     "Papelaria"]
 
-    let produtos: Profile[]
-    produtos = [
+    const [produtos, setProdutos] = useState<Profile[]>([]);
+
+    //let produtos: Profile[]
+    /*produtos = [
       {
         titulo: "Sabão",
         id: 0,
@@ -40,7 +42,17 @@ export function Form(){
         valor: 2.50,
         descricao: "Comida"
       },
-    ]
+    ]*/
+
+    const [count, setCount] = useState(0);
+
+    const {register, handleSubmit, formState: { errors }} = useForm<Profile>()
+
+    const cadastro = handleSubmit((data) => {
+      data.id = count
+      produtos[count] = data;
+      setCount(count + 1)
+    })
 
   return(
     <div className="main-index">
@@ -49,28 +61,40 @@ export function Form(){
         <p className="title">Cadastro de Produtos</p>
         <div className="nav">
           <nav id="tab-form">
-              <form className="data-input">
+              <form className="data-input" onSubmit={cadastro}>
                   <div className="form-input">
                     <div className="form-container">
                       <label className="titulo">Título:</label>
-                      <input type="text" id="titulo" className="inputTitulo" placeholder="Digite o Titulo do Produto" />
+                      <input {...register("titulo", {required: true})} type="text" id="titulo" className="inputTitulo" placeholder="Digite o Titulo do Produto" />
+                      {
+                        errors.titulo && <div className="error"> É necessário informar o título do Produto</div>
+                      }
                     </div>
                     <div className="form-container">
                       <label className="categoria" >Categoria:</label>
-                      <select name="categoria" id="categoria">
+                      <select {...register("categoria", {required: true})} name="categoria" id="categoria">
                           <option value="">Selecione a categoria do Produto</option>
                           {categorias.map((id, index) => 
                             <option> {categorias[index]} </option>)
                           }
                       </select>
+                      {
+                        errors.categoria && <div className="error"> É necessário selecionar a categoria do Produto</div>
+                      }
                     </div>
                     <div className="form-container">
                       <label className="valor">Valor:</label>
-                      <input type="text" id="valor" className="inputValor" placeholder="Digite o Preço do Produto em R$" />
+                      <input  {...register("valor", {required: true})} pattern="[0-9]+" type="text" id="valor" className="inputValor" placeholder="Digite o Preço do Produto em R$" />
+                      {
+                        errors.valor && <div className="error"> É necessário informar o Valor do Produto</div>
+                      }
                     </div>
                     <div className="form-container">
                       <label className="label-descricao" >Descrição:</label>
-                      <textarea id="descricao" className="textADescricao" />
+                      <textarea  {...register("descricao", {required: true})} id="descricao" className="textADescricao" />
+                      {
+                        errors.descricao && <div className="error"> É necessário informar a descrição do Produto</div>
+                      }
                     </div>
                   </div>
                   <div className="form-btn">
